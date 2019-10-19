e2ecedd2e188559fb6f25c767b4a547cbf03e641
Angelos Perivolaropoulos <angelos.perivolaropoulos@skyscanner.net>
2019-10-19 22:33:44 +0100

    Improvements

--- src/run.py
+++ src/run.py
@@ -29,8 +29,8 @@ from src.watson import WatsonLang
 
 import json
 
-account_sid = "AC56ad713dba7476d55ae0fe3343ead6ff"
-auth_token = "de57cf731c46e9c38f92560d63cca103"
+account_sid = "ACb7774b0b683f617feea02e6d667beb3c"
+auth_token = "97d69f73e6d9b03a9ba17b895d7474b5"
 client = Client(account_sid, auth_token)
 
 # The session object makes use of a secret key.
@@ -41,9 +41,10 @@ app.config.from_object(__name__)
 # Try adding your own number to this list!
 callers = {
     "Angelos": "+447484330666",
-    "Leo": "+447484330666",
+    "Leo": "+447519451663",
     "Geo": "+447598734567",
-    "Andy": "447519451663"
+    "Andy": "+447519451663",
+    "Jordan": "+447588434313"
 }
 
 state = {}
@@ -52,7 +53,7 @@ def send_sms(to_num, text):
     message = client.messages \
                 .create(
                      body=text,
-                     from_='+15024132587',
+                     from_='+441297533050',
                      to=to_num
                  )
 
@@ -77,12 +78,15 @@ def hello():
         state['participants'] = []
 
     else:
-        if not participants:
+        if not state['participants']:
             raise
-
-        participants.remove(from_number)
-
-        send_sms(participants[0], text)
+        if from_number == state['participants'][0]:
+            to_number = state['participants'][1]
+        else:
+            to_number = state['participants'][0]
+        send_sms(to_number, text)
+    print(state)
+    
 
     # Build our reply
     message = json.dumps(emotions)
